abf36b5fee69887a273b27e927c36848b1b2d34e
Sandro <sandrofillinich@gmail.com>
2021-06-16 06:09:07 +0100

    Improved preloading animation

--- src/components/loadingScreen.js
+++ src/components/loadingScreen.js
@@ -7,9 +7,17 @@ const LoadingScreen = () => {
     const { canvasIsCreated } = useContext(StateContext)
     return (
         <div id="loading-screen" className={`loading-screen${canvasIsCreated ? " loading-screen--fade-out" : ""}`}>
-            <div className="loading-screen__content">
+            
+                <div className="loading-screen__logo__half">
+                    <div className="loading-screen__logo__half-first">
+                        S<span>ANDRO</span>
+                    </div> 
+                </div>
                 <div className="loading-screen__white-flash"></div>
-            </div>
+                <div className="loading-screen__logo__half">
+                    <span style={{color: "red"}}>Fi.</span>
+                </div>
+
         </div>
     )
 }
